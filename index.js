f9efac29a519002c768b21e2aa089e625a9d2a8f
BrunoPrdt <bruno.predot@gmail.com>
2020-07-27 21:24:08 +0200

    add square selection

--- index.js
+++ index.js
@@ -10,11 +10,25 @@ let vm = new Vue({
         squareMapping: ["hautGauche", "hautDroite", "basGauche", "basDroite"]
     },
     methods: {
+        allGray() {
+            this.hautGauche = false;
+            this.hautDroite = false,
+            this.basGauche = false,
+            this.basDroite = false
+        },
         addNewElemToSequence() {
             this.sequence.push(this.squareMapping[Math.floor(Math.random() * 4)]);
         },
         newGame() {
+            this.sequence = [];
             this.addNewElemToSequence();
-        }
+            this[this.sequence[0]] = true;
+            setTimeout(() => {
+                this.allGray();
+            }, 500);
+        },
+        selectSquare(carre) {
+            console.log(carre);
+        },
     },
 });
